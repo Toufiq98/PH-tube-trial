0df05d66086bbe62bf9718db94bb714af33b0405
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-03-15 14:32:36 +0600

    modal added with dynamic value

--- script/index.js
+++ script/index.js
@@ -35,6 +35,38 @@ const loadCategoryVideos = (id) => {
         displayVideos(data.category)
     });
 }
+
+const loadVideoDetails = (videoId) => {
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+    console.log(video);
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML=
+    `
+    <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">"${video.title}"</h2>
+    <p>"${video.description}"</p>
+    <div class="card-actions justify-end">
+     
+    </div>
+  </div>
+</div>
+    `
+};
+
 function displayCategories(categories) {
     const categoryContainer = document.getElementById("category-container");
     for(let cat of categories) {
@@ -90,7 +122,7 @@ const displayVideos = (videos) => {
                     <p class="text-sm text-gray-400">${video.others.views}views</p>
                 </div>
             </div>    
-              
+             <button onclick=loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button> 
         </div>
 
         `;
